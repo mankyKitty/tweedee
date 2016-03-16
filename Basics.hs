4de96110d2c482e490535271df97e2c27aab2083
Sean Chalmers <mankyKitty@users.noreply.github.com>
2016-03-16 19:42:01 +1000

    Update Basics.hs
    Tweaking tweaking, rotation functions are incorrect

--- Basics.hs
+++ Basics.hs
@@ -30,5 +30,51 @@ subtractVectorFromVector (Vector (a,b,c)) (Vector (d,e,f)) =
 drawPoint :: Point -> IO ()
 drawPoint = print
 
+rotatePointXY :: Point -> Degrees -> Point
+rotatePointXY (Point (a,b,c)) d =
+	Point (
+		(cos d * a) + ((negate $ sin d) * a),
+		(sin d * b) + (cos d * b),
+		c
+	)
+
+rotatePointXZ :: Point -> Degrees -> Point
+rotatePointXZ (Point (a,b,c)) d =
+	Point (
+		(cos d * a) + (sin d * a),
+		b,
+		((negate $ sin d) * c) + (cos d * c)
+	)
+
+rotatePointYZ :: Point -> Degrees -> Point
+rotatePointYZ (Point (a,b,c)) d =
+	Point (
+		a,
+	  	(cos d * b) + ((negate $ sin d) * b),
+	  	(sin d * c) + (cos d * c)
+	)
+
+scalePoint :: Point -> Scale -> Point
+scalePoint (Point (a,b,c)) (Scale (x,y,z)) =
+	Point (a * x, b * y, c * z)
+
+main :: IO ()
+main = do
+	let p1 = Point (X 1.0, Y 2.0, Z 1.0)
+	let p2 = Point (X 0.0, Y 4.0, Z 4.0)
+	let v1 = Vector (X 2.0, Y 0.0, Z 0.0)
+	
+    drawPoint p1 -- should display (1,2,1)
+    drawPoint p2 -- should display (0,4,4)
+
+	let v2 = subtractPointFromPoint p1 p2
+	let v1' = addVectorToVector v1 v2
+	
+	let p1' = addVectorToPoint p1 v1'
+    drawPoint p1' -- should display (4,0,-2)
+
+	let p2' = subtractVectorFromPoint p2 v2
+    drawPoint p2' -- should display (-1,6,7)
+
 axiomOneAndTwo :: Point -> Point -> Bool
 axiomOneAndTwo p q = let v = p - q in q == p + v
